1f55d2395a602fdb5c5fa4f851e5e0cce294268b
Prem Jatol <premjatol@gmail.com>
2025-07-14 22:00:36 +0530

    fs repeat

--- create server using node.js (http)/index.js
+++ create server using node.js (http)/index.js
@@ -6,17 +6,22 @@ const http = require('http');
 // Express.js -> node package-> req-res cycle
 // req.body -> data
 
-const server = http.createServer(
-    (req, res) => {
-        if(req.url === '/' && req.method === 'GET'){
-            res.end("this is home page")
-        }else if(req.url === '/edit-user' && req.method === 'POST'){
-            res.end("user eddited")
-        }else if(req.url === '/edit-user' && req.method === 'GET'){
-            res.end("user eddited from get method")
-        }
-    }
-)
+const server = http.createServer((req, res)=> {
+    console.log("req url:", req.url)
+    res.end("hello world")
+});
+
+// const server = http.createServer(
+//     (req, res) => {
+//         if(req.url === '/' && req.method === 'GET'){
+//             res.end("this is home page")
+//         }else if(req.url === '/edit-user' && req.method === 'POST'){
+//             res.end("user eddited")
+//         }else if(req.url === '/edit-user' && req.method === 'GET'){
+//             res.end("user eddited from get method")
+//         }
+//     }
+// )
 
 server.listen(5000, (err)=> {
     if(err){
