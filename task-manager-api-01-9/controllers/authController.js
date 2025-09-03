445c9aa428adcf8263c87b66bbb8d1143ab6560a
prem-jatol <premjatolblogger@gmail.com>
2025-09-03 22:40:06 +0530

    auth model completed

---
+++ task-manager-api-01-9/controllers/authController.js
@@ -0,0 +1,27 @@
+const User = require("./../models/Auth");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    // const user = new User({req.body});
+    // user.save()
+    res.status(202).json({ message: "User registered", user });
+  } catch (err) {
+    res.status(400).json({ error: err.message || "internal server error" });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  
+  if (!user) return res.status(400).json({ error: "Invalid email" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ error: "Invalid password" });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+  res.json({ message: "Login successful", token, user });
+};
--- task-manager-api-01-9/server.js
+++ task-manager-api-01-9/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/auth", require("./routes/authRouter"));
 // app.use("/api/tasks", require("./routes/taskRoutes"));
 
 const PORT = process.env.PORT || 5000;
